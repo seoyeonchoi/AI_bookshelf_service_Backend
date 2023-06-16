704a1862c8a49e5cae51ed3c27a0e6c026287744
kim-deokju <edaumedo1@naver.com>
2023-06-16 09:26:31 +0900

    install cors

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ import mongoose from "mongoose";
 import Express from "express";
 import cors from 'cors';
 // import bodyParser from "body-parser";
+import cors from "cors";
 
 dotenv.config();
 const app = Express();
-const localhost = 'localhost'
+const localhost = "localhost";
 const PORT = process.env.PORT || 5000; // 포트번호
 
 app.use(cors())
 // json 파싱은 기본 100kb까지만 가능하다 limit는 이를 50MB까지 하기 위한 옵션이다
-app.use(Express.urlencoded({ limit: "50mb", extended: true }));
-app.use(Express.json({ limit: "50mb" }));
+app.use(Express.urlencoded({ extended: true }));
+app.use(Express.json());
 
 app.get("/", (req, res) => {
   res.send(`Okay let's go`);
@@ -25,7 +26,9 @@ app.use("/auth", AuthRouter);
 // app.use("/user", UserRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server on : http://${process.env.REMOTE_IP || localhost}:${PORT}/`);
+  console.log(
+    `Server on : http://${process.env.REMOTE_IP || localhost}:${PORT}/`
+  );
 });
 
 //mongodb
