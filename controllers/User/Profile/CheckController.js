2571e5550678fbb982b5aafaaf8f5e6669247bae
kim-deokju <edaumedo1@naver.com>
2023-07-04 14:29:27 +0900

    .

--- controllers/User/Profile/CheckController.js
+++ controllers/User/Profile/CheckController.js
@@ -5,37 +5,37 @@ const aggregatePipeline = [
     $facet: {
       book_data: [
         { $project: { user_bookshelf: 1 } },
-        { $unwind: '$user_bookshelf' },
+        { $unwind: "$user_bookshelf" },
         {
           $group: {
             _id: null,
-            page_sum: { $sum: '$user_bookshelf.page' },
-            weight_sum: { $sum: '$user_bookshelf.weight' },
-            data: { $push: '$$ROOT' },
+            page_sum: { $sum: "$user_bookshelf.page" },
+            weight_sum: { $sum: "$user_bookshelf.weight" },
+            data: { $push: "$$ROOT" },
           },
         },
         {
           $project: {
             page_sum: 1,
             weight_sum: 1,
-            page_mean: { $divide: ['$page_sum', { $size: '$data' }] },
-            weight_mean: { $divide: ['$weight_sum', { $size: '$data' }] },
+            page_mean: { $divide: ["$page_sum", { $size: "$data" }] },
+            weight_mean: { $divide: ["$weight_sum", { $size: "$data" }] },
             data: 1,
           },
         },
-        { $unwind: '$data' },
+        { $unwind: "$data" },
         {
           $group: {
-            _id: '$data.user_bookshelf.category',
+            _id: "$data.user_bookshelf.category",
             count: { $sum: 1 },
-            data: { $push: '$data' },
+            data: { $push: "$data" },
           },
         },
         { $sort: { count: -1 } },
         { $limit: 1 },
         {
           $project: {
-            maxCategory: '$_id',
+            maxCategory: "$_id",
             count: 1,
             data: 1,
           },
@@ -43,10 +43,10 @@ const aggregatePipeline = [
       ],
       maxAuthor: [
         { $project: { user_bookshelf: 1 } },
-        { $unwind: '$user_bookshelf' },
+        { $unwind: "$user_bookshelf" },
         {
           $group: {
-            _id: '$user_bookshelf.author',
+            _id: "$user_bookshelf.author",
             count: { $sum: 1 },
           },
         },
@@ -54,23 +54,23 @@ const aggregatePipeline = [
         { $limit: 1 },
         {
           $project: {
-            maxAuthor: '$_id',
+            maxAuthor: "$_id",
             count: 1,
           },
         },
       ],
       category: [
-        { $unwind: '$user_bookshelf' },
+        { $unwind: "$user_bookshelf" },
         {
           $group: {
-            _id: '$user_bookshelf.category',
+            _id: "$user_bookshelf.category",
             categoryCount: { $sum: 1 },
           },
         },
         {
           $group: {
             _id: null,
-            category: { $push: { category: '$_id', count: '$categoryCount' } },
+            category: { $push: { category: "$_id", count: "$categoryCount" } },
           },
         },
         {
@@ -82,36 +82,33 @@ const aggregatePipeline = [
     },
   },
   {
-    $unwind: '$book_data',
+    $unwind: "$book_data",
   },
   {
     $unwind: {
-      path: '$maxAuthor',
+      path: "$maxAuthor",
       preserveNullAndEmptyArrays: true,
     },
   },
 
-  
   {
     $project: {
-      page_sum: '$book_data.data.page_sum',
-      page_mean: '$book_data.data.page_mean',
-      weight_sum: '$book_data.data.weight_sum',
-      weight_mean: '$book_data.data.weight_mean',
-      maxCategory: '$book_data.maxCategory',
-      maxAuthor: { $ifNull: ['$maxAuthor.maxAuthor', null] },
-          category: {
-            $cond: {
-              if: { $isArray: '$category.category' },
-              then: { $arrayElemAt: ['$category.category', 0] },
-              else: [],
-            },
-          },
+      page_sum: "$book_data.data.page_sum",
+      page_mean: "$book_data.data.page_mean",
+      weight_sum: "$book_data.data.weight_sum",
+      weight_mean: "$book_data.data.weight_mean",
+      maxCategory: "$book_data.maxCategory",
+      maxAuthor: { $ifNull: ["$maxAuthor.maxAuthor", null] },
+      category: {
+        $cond: {
+          if: { $isArray: "$category.category" },
+          then: { $arrayElemAt: ["$category.category", 0] },
+          else: [],
         },
       },
-    ];
-
-
+    },
+  },
+];
 
 // 집계 목록 전달기능
 export const Check = async (req, res) => {
@@ -130,7 +127,7 @@ export const Check = async (req, res) => {
       // console.log(list);
       return res.status(200).json({
         success: true,
-        info: aggregatePipeline
+        info: aggregatePipeline,
       });
     });
     // console.log(req.body);
